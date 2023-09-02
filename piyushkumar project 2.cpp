57ed7bb02585f597775a39c1559a564968bb337c
PiyushKmar <143862095+PiyushKmar@users.noreply.github.com>
2023-09-02 12:43:33 +0530

    Add files via upload
    Basic programs of C assignment 2

---
+++ piyushkumar project 2.cpp
@@ -0,0 +1,70 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+	float l,b;
+	printf("enter lenght and breadth");
+	scanf("%f%f",&l,&b);
+	float a=l*b;
+	float d=2*(l+b);
+	printf("area is :%.2f\nperimeter is :%.2f",a,b);
+	
+	return 0;
+}
+
+
+	
+int main()
+{
+
+	
+	float a,b,c;
+	printf("enter both vale:");
+	scanf("%f%f",&a,&b);
+	printf("before swaping value %.2f,%.2f",a,b);
+	c=a;
+	a=b;
+	b=c;
+	printf("\nafter swapin value %.2f,%.2f",a,b);
+}
+
+
+
+
+int main()
+{
+
+	
+	float a,b;
+	printf("Enter the value ");
+	scanf("%f%f",&a,&b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("after swaping number %.2f,%.2f",a,b);
+
+
+}
+
+
+int main()
+{
+
+    printf("size of char : %d",sizeof(char));
+	printf("\nsize of int : %d",sizeof(int));
+	printf("\nsize of float : %d",sizeof(float));
+	printf("\nsize of double : %d",sizeof(double));
+	printf("\nsize of long : %d",sizeof(long));
+	printf("\nsize of short int : %d",sizeof(short int));
+	
+	return 0;
+}
+	
+
+	
+	
+
+
